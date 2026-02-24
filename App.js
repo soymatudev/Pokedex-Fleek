f0c50d415529ad53be93e425517a78f692566d9f
SoyMatudev <juanjesusgarciamaturana@gmail.com>
2026-02-23 20:38:48 -0600

    Chore: Clean App

--- App.js
+++ App.js
@@ -1,25 +1,8 @@
 import React from 'react';
-
-import { NavigationContainer } from '@react-navigation/native';
-
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import AppRouter from './src/navigation/AppRouter';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <AppRouter />
-    </SafeAreaProvider>
+    <AppRouter />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
