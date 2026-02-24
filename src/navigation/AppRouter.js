da7f3a9ca734277d187b046e9ee96738055ae21e
SoyMatudev <juanjesusgarciamaturana@gmail.com>
2026-02-23 20:39:23 -0600

    Chore: Navigation routes

--- src/navigation/AppRouter.js
+++ src/navigation/AppRouter.js
@@ -5,13 +5,14 @@ import { NavigationContainer } from '@react-navigation/native';
 import { HomeScreen } from '../features/home/HomeScreen';
 import { ScannerScreen } from '../features/scanner/ScannerScreen';
 import { DetailsScreen } from '../features/pokedex/DetailsScreen';
+import { PokedexListScreen } from '../features/pokedex/PokedexListScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function AppRouter() {
   return (
     <NavigationContainer>
-      <Stack.Navigator 
+      <Stack.Navigator
         initialRouteName="Home"
         screenOptions={{
           headerStyle: { backgroundColor: '#f44336' }, // Rojo Pokémon
@@ -20,8 +21,9 @@ export default function AppRouter() {
         }}
       >
         <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'PokéDex' }} />
-        {/* <Stack.Screen name="Scanner" component={ScannerScreen} options={{ title: 'Escáner de Campo' }} />
-        <Stack.Screen name="Details" component={DetailsScreen} options={{ title: 'Datos de la Pokédex' }} /> */}
+        {/* <Stack.Screen name="Scanner" component={ScannerScreen} options={{ title: 'Escáner de Campo' }} /> */}
+        <Stack.Screen name="Details" component={DetailsScreen} options={{ title: 'Datos de la Pokédex' }} />
+        <Stack.Screen name="PokedexList" component={PokedexListScreen} options={{ title: 'Lista de Pokémon' }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
